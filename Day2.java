3d3d3e675d40e9a139939215b5620b13747f5c4f
Vai11sh <99194605+Vai11sh@users.noreply.github.com>
2023-04-01 17:13:28 +0530

    Add files via upload

---
+++ Day2.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Day2 {
+    public static void main(String args[])
+         {
+            int a , b;
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the First Number:");
+            a = sc.nextInt();
+
+            System.out.println("Enter the Second Number:");
+            b = sc.nextInt();
+
+           int  result = a+b;
+
+            System.out.println("the sum of two integer is "+ result);
+           
+         
+         }
+        }
+         
